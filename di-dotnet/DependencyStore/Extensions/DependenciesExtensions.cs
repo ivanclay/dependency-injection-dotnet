4875214fa22d16f6efbb79a4709b9dc878748819
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-25 21:14:58 -0300

    feat: add extension methods

---
+++ di-dotnet/DependencyStore/Extensions/DependenciesExtensions.cs
@@ -0,0 +1,26 @@
+﻿using DependencyStore.Repositories.Contracts;
+using DependencyStore.Repositories;
+using Microsoft.Data.SqlClient;
+using DependencyStore.Services.Contracts;
+using DependencyStore.Services;
+
+namespace DependencyStore.Extensions;
+
+public static class DependenciesExtensions
+{
+    public static void AddSqlConnection(this IServiceCollection services, string connection) 
+    {
+        services.AddScoped<SqlConnection>(x => new SqlConnection(connection));
+    }
+
+    public static void AddRepositories(this IServiceCollection services) 
+    {
+        services.AddTransient<ICustomerRepository, CustomerRepository>();
+        services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+    }
+
+    public static void AddServices(this IServiceCollection services) 
+    {
+        services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+    }
+}
